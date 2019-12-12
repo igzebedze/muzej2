9a5b639d3825f5d67edb6c5ae177132c4edd63d3
bostjan spetic <bostjan@konstrukt.it>
2019-12-12 17:43:06 +0100

    user select

--- inventura/management/commands/import_sw.py
+++ inventura/management/commands/import_sw.py
@@ -35,14 +35,13 @@ class Command(BaseCommand):
 				proizvajalec = row['proizvajalec'].capitalize()
 				self.stdout.write("looking at %s %s %s" % (proizvajalec, eksponat, letnik))
 
-				popisovalec = row['popisovalec']
-				if popisovalec:
-					try:
-						u = User.objects.get(username=popisovalec)
-					except:
-						pass
-					else:
-						user = u
+				try:
+					popisovalec = row['popisovalec']
+					u = User.objects.get(username=popisovalec)
+				except:
+					pass
+				else:
+					user = u
 
 				if not eksponat:
 					continue;
