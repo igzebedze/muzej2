4c26117080b36b7da98c5c6e0d989b8b4c0b9232
bostjan spetic <bostjan@konstrukt.it>
2021-03-27 07:55:11 +0100

    admin

--- inventura/admin.py
+++ inventura/admin.py
@@ -92,6 +92,7 @@ class PrimerekInline(admin.TabularInline):
 	model = models.Primerek
 	fields = ('inventarna_st', 'eksponat', 'serijska_st', 'leto_proizvodnje', 'inventariziral')
 	readonly_fields = ('inventarna_st', 'eksponat', 'serijska_st', 'leto_proizvodnje', 'inventariziral')	
+	date_hiearchy = 'datum_inventarizacije'
 	extra = 0
 
 class PregledInline(admin.TabularInline):
