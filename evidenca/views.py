0c8717982d9df20ff722d4a334883d7370df7bfb
bostjan <bostjan@konstrukt.it>
2022-08-19 18:27:47 +0200

    view

--- evidenca/views.py
+++ evidenca/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from evidenca.models import *
 
-# Create your views here.
+class RacunalnikListView(ListView):
+    model = racunalnik
+
+class OrganizacijeListView(ListView):
+    model=organizacija
