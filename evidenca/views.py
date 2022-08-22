9b72f0b6be03c60a1fb36b4e87f869fc955204f5
bostjan <bostjan@konstrukt.it>
2022-08-22 21:01:04 +0200

    evidenca poc

--- evidenca/views.py
+++ evidenca/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from evidenca.models import *
 
 class RacunalnikListView(ListView):
     model=racunalnik
     queryset=racunalnik.objects.order_by('nakup')
+
+class RacunalnikDetailView(DetailView):
+    model=racunalnik
 
 class OrganizacijeListView(ListView):
     model=organizacija
