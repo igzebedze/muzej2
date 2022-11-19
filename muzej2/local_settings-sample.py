ba38e89dbf1a417d447ee452f7208c6ccc99ed05
Anej Gorkič <anej.gorkic@gmail.com>
2022-11-19 20:21:33 +0100

    evidenca and organizacije style

--- muzej2/local_settings-sample.py
+++ muzej2/local_settings-sample.py
@@ -1,4 +1,5 @@
 import os
+from django.core.management.utils import get_random_secret_key  
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -7,7 +8,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
