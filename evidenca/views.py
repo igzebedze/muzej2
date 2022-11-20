e50d3f011407c911794a474a688ec71c23004628
bostjan <bostjan@konstrukt.it>
2022-11-20 22:07:13 +0100

    forgot

--- evidenca/views.py
+++ evidenca/views.py
@@ -6,9 +6,28 @@ class RacunalnikListView(ListView):
     model=racunalnik
     queryset=racunalnik.objects.order_by('nakup')
 
+class RacunalnikiGeoJsonView(ListView):
+    model=racunalnik
+    queryset=racunalnik.objects.order_by('nakup')
+    template_name='evidenca/racunalniki.geojson'
+
 class RacunalnikDetailView(DetailView):
     model=racunalnik
 
 class OrganizacijeListView(ListView):
     model=organizacija
     queryset=organizacija.objects.order_by('ime')
+
+def stat(request):
+	generacije = []	
+	
+	context = {
+        'objekti': {
+            'viri': vir.objects.count,
+            'racunalniki': racunalnik.objects.count,
+            'organizacije': organizacija.objects.count
+        },
+        'racunalniki': racunalnik.objects,
+        'racunalniki_po_generacijah': generacije
+	}
+	return render(request, 'stat.html', context)
