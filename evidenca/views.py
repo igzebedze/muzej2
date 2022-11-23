cebdbd9bfba4ac4a51264df43d57ee47f934b4a8
Anej Gorkič <anej.gorkic@gmail.com>
2022-11-23 20:31:44 +0100

    Missing view

--- evidenca/views.py
+++ evidenca/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView
+from django.core import serializers
 from evidenca.models import *
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 class RacunalnikListView(ListView):
     model=racunalnik
@@ -11,9 +14,6 @@ class RacunalnikiGeoJsonView(ListView):
     queryset=racunalnik.objects.order_by('nakup')
     template_name='evidenca/racunalniki.geojson'
 
-class RacunalnikDetailView(DetailView):
-    model=racunalnik
-
 class OrganizacijeListView(ListView):
     model=organizacija
     queryset=organizacija.objects.order_by('ime')
@@ -23,3 +23,15 @@ class RacunalnikiGeoJsonView(ListView):
     queryset=racunalnik.objects.order_by('nakup')
     template_name='evidenca/racunalniki.geojson'
 
+# class RacunalnikSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = racunalnik
+#         fields = ['pk', 'nosilec','organizacija','ime','tip','opombe','proizvajalec','nakup', 'opis', 'generacija', 'viri', 'kraj', 'lastnistvo']
+
+
+@api_view(['GET', 'POST'])
+def racunalnik_detail(request, pk):
+    if request.method == 'GET':
+        data = serializers.serialize("json", racunalnik.objects.all().prefetch_related().filter(pk=pk))
+        # //serializer = RacunalnikSerializer(data, many=True)
+        return Response(data)
