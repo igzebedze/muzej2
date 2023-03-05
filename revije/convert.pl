72102dbecb1257d38f0a6a9179892f4379ffa6a2
bostjan <bostjan@konstrukt.it>
2023-03-05 10:00:33 +0100

    bacground alpha

--- revije/convert.pl
+++ revije/convert.pl
@@ -3,12 +3,14 @@
 use strict;
 my $homedir = "pc.sux.org.nosync";
 
+# convert -density 300 file.pdf[0] -resize 80x80 -background white -alpha remove -strip -quality 70 thumb.jpg
+
 foreach my $file (sort readpipe("find $homedir -name '*.gif'")) {
 	chop $file;
 	#warn $file;
 	my $target = $file;
 	$target =~ s/\.gif//;
-	system("convert '$file' '$target.jpg'") if not -f "$target.jpg"; 
+	system("convert '$file' -background white -alpha remove '$target.jpg'") if not -f "$target.jpg"; 
 }
 
 foreach my $file (sort readpipe("find $homedir -name '*.JPG'")) {
