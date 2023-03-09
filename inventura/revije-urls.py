7511471ee21db14b9787b8692fc037578e8c73ed
bostjan <bostjan@konstrukt.it>
2023-03-09 21:11:54 +0100

    blah

--- inventura/revije-urls.py
+++ inventura/revije-urls.py
@@ -6,11 +6,11 @@ from inventura.views import *
 
 urlpatterns = [
     path('', revijeYearsView.as_view()),
-    path('<str:tip>/', revijaYearsView),
     path('oprojektu/', TemplateView.as_view(template_name='inventura/revijeoprojektu.html'), name='oprojektu'), 
     path('revije/<int:pk>/js', revijaJSView),
     path('revije/<int:pk>/thumbs', revijaThumbsView),
     path('revije/<int:pk>/', revijaView.as_view()),
+    path('<str:tip>/', revijaYearsView),
 ] 
 if settings.DEBUG is True:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
