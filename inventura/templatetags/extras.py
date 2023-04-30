138347574c781d3c6bde1911d95740afaf4f041d
bostjan <bostjan@konstrukt.it>
2023-04-30 17:43:27 +0200

    bug

--- inventura/templatetags/extras.py
+++ inventura/templatetags/extras.py
@@ -12,6 +12,8 @@ def add_zeros(mixed):
     return "{:02d}".format(int(''.join(filter(str.isdigit, mixed))))
 
 @register.inclusion_tag('inventura/revije_nav_tag.html')
-def revije_nav(tip, root):
+def revije_nav(tip, root, selected):
     object_list = Tiskovina.objects.all()
-    return {'object_list': object_list, 'selected': tip, 'root': root }
+    if not selected:
+        selected = object_list
+    return {'revije': object_list, 'selected': tip, 'root': root, 'object_list': selected }
