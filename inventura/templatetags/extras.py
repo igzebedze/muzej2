e6d7aeaaccb3d8ce6b49e442989b483d7a3c3175
bostjan <bostjan@konstrukt.it>
2023-04-30 15:21:23 +0200

    about template

--- inventura/templatetags/extras.py
+++ inventura/templatetags/extras.py
@@ -1,4 +1,5 @@
 from django import template
+from inventura.models import Tiskovina
 
 register = template.Library()
 
@@ -9,3 +10,8 @@ def to_unicode(mixed):
 @register.filter
 def add_zeros(mixed):
     return "{:02d}".format(int(''.join(filter(str.isdigit, mixed))))
+
+@register.inclusion_tag('inventura/revije_nav_tag.html')
+def revije_nav(tip):
+    object_list = Tiskovina.objects.all()
+    return {'object_list': object_list, 'selected': tip}
